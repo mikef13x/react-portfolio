1beeb6d541fa96aff9754cf521fa316a51343dc7
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-07-12 14:35:23 -0400

    changed picture location to local

--- src/components/about/picture.jsx
+++ src/components/about/picture.jsx
@@ -19,7 +19,7 @@ export default function Picture() {
                 marginLeft: '300px'
                
             }} >
-            <img src="https://media.discordapp.net/attachments/1253520612459282453/1260039164062208040/mike_about_me_1.jpg?ex=668dde6b&is=668c8ceb&hm=8fc0f5e6b0cf50fa8df3078188d8944c547b20a989d9959dba43abf4c0cedeec&=&format=webp&width=904&height=904" alt="Name 1" style={{  maxWidth: '600px', maxHeight: '600px'}} />
+            <img src="../../../assets/images/mike_about_me_1.jpg" alt="Name 1" style={{  maxWidth: '600px', maxHeight: '600px'}} />
             </Box>
             <Typography variant="h4" component="h2" item  sx={{ marginBottom: '20px', marginLeft:'250px', textAlign: 'center', color: 'white', marginTop: '20px' }}>
                 <span className="montserrat"> Michael Freeman</span>
