c1c50ec6282916db9f9bb483d9ae6a82af8ba609
mikef13x <mikef132044@gmail.com>
2024-07-15 23:48:49 -0400

    added proper projects and descriptions, links, and photos!

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -2,80 +2,58 @@
 import React from "react"
 import PortfolioSection from "../components/portfolio/portfoliosection"
 import Aniname from "../assets/images/Screenshot 2024-03-25 190554.png"
-import IndieIgnite from "../assets/images/stock-vector-vector-illustration-neon-future-game-pad-background-1861318969.jpg"
+import IndieIgnite from "../assets/images/indieignite.png"
+import CodingQuiz from '../assets/images/site screenshot.png'
+import PasswordGenerator from '../assets/images/passwordgenerator.png'
+import WeatherApp from '../assets/images/weatherapp.png'
+import DebateCafe from '../assets/images/debatecafe.png'
 
 const portfolioCards = [
     {
         _id: '1',
         title: 'Indie Ignite',
-        description: 'this is indie ignite lorem ipsum , indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ip',
+        description: 'This is Indie Ignite, a platform for indie game developers to share their game in the making, gather funding and support in hopes for a more successful launch!',
         link: 'https://github.com/SpencerKlink/Indie-Ignite',
-        image: Aniname
+        image: IndieIgnite
     },  
     {
         _id: '2',
         title: 'Aniname',
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        link: 'https://github.com/mikef13x/project-1-null',
-        image: IndieIgnite
+        description: "This is Aniname, a site for anime enjoyers to search for new shows, create a list of their current watched shows, or future watches",
+        link: 'https://mikef13x.github.io/project-1-null/',
+        image: Aniname
     },
     {
         _id: '3',
-        title: 'Indie Ignite',
-        description: 'this is indie ignite',
-        link: 'https://github.com/SpencerKlink/Indie-Ignite',
-        image: Aniname
-    },  
+        title: 'Debate cafe',
+        description: "This is Debate Cafe, a site for users to discuss hot topics or create their own!",
+        link: 'https://debate-cafe.onrender.com/',
+        image: DebateCafe
+    },
     {
         _id: '4',
-        title: 'Aniname',
-        description: 'this is indie ignite lorem ipsum , indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ip',
-     
-        link: 'https://github.com/mikef13x/project-1-null',
-        image: IndieIgnite
-    },
+        title: 'Coding Quiz',
+        description: 'This is a coding quiz where users can test their knowledge on the basics of HTML, CSS and JavaScript!',
+        link: 'https://mikef13x.github.io/coding-quiz/',
+        image: CodingQuiz
+    },  
     {
         _id: '5',
-        title: 'Indie Ignite',
-        description: 'this is indie ignite lorem ipsum , indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ip',
-        link: 'https://github.com/SpencerKlink/Indie-Ignite',
-        image: Aniname
-    },  
+        title: 'Password Generator',
+        description: 'Users can choose a password between 8-128 characters. They can choose to include uppercase, lowercase, numbers, and special characters, and a password will be randomly generated!',
+     
+        link: 'https://mikef13x.github.io/password-generator/',
+        image: PasswordGenerator
+    },
     {
         _id: '6',
-        title: 'Aniname',
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        link: 'https://github.com/mikef13x/project-1-null',
-        image: IndieIgnite
-    },
-    {
-        _id: '7',
-        title: 'Indie Ignite',
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        link: 'https://github.com/SpencerKlink/Indie-Ignite',
-        image: Aniname
+        title: 'Weather App',
+        description: 'This is a Weather App where users can find the current weather and 5 day forecast of any city they choose!',
+        link: 'https://mikef13x.github.io/weather-app/',
+        image: WeatherApp
     },  
-    {
-        _id: '8',
-        title: 'Aniname',
-        description: 'this is indie ignite lorem ipsum , indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ip',
-        link: 'https://github.com/mikef13x/project-1-null',
-        image: IndieIgnite
-    },
-    {
-        _id: '9',
-        title: 'Indie Ignite',
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        link: 'https://github.com/SpencerKlink/Indie-Ignite',
-        image: Aniname
-    },  
-    {
-        _id: '10',
-        title: 'Aniname',
-        description: 'this is indie ignite lorem ipsum , indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ipsumindie ignite lorem ipsum, indie ignite lorem ip',
-        link: 'https://github.com/mikef13x/project-1-null',
-        image: IndieIgnite
-    },
+   
+  
 
 ]
 
